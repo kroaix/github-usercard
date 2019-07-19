bff35e2e1147436a15902be2335c5c20c5b04080
Katrina Roaix <kroaix@gmail.com>
2019-07-19 00:36:30 -0400

    MVP Complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,18 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/kroaix')
+  .then((data) => {
+    console.log('Success!', data);
+    const cards = document.querySelector('.cards');
+    cards.appendChild(githubCard(data.data));
+  })
+  .catch((err) => {
+    console.log('Error: ', err);
+  })  
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -18,13 +29,76 @@
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
           at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
+          Individual strings to the followersArray below.
           
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['MicahJank', 'Heather-Ridgill', 'meholt', 'ryanboris', 'lilvina', 'tetondan'];
+
+followersArray.forEach((follower) => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then((data) => {
+      console.log('Success!', data);
+      const cards = document.querySelector('.cards');
+      cards.appendChild(githubCard(data.data));
+    })
+    .catch((err) => {
+      console.log('Error: ', err);
+    })  
+})
+
+const githubCard = (user) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const img = document.createElement('img');
+  img.src = user.avatar_url;
+  card.appendChild(img);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  card.appendChild(cardInfo);
+
+  const name = document.createElement('h3');
+  name.classList.add('name');
+  name.textContent = user.name;
+  cardInfo.appendChild(name);
+
+  const userName = document.createElement('p');
+  userName.classList.add('username');
+  cardInfo.appendChild(userName);
+
+  const location = document.createElement('p');
+  location.textContent = `Location: ${user.location}`;
+  cardInfo.appendChild(location);
+
+  const profile = document.createElement('p');
+  profile.textContent = "Profile: ";
+  cardInfo.appendChild(profile);
+
+  const profileLink = document.createElement('a');
+  profileLink.href = user.html_url;
+  profileLink.textContent = user.html_url;
+  profile.appendChild(profileLink);
+
+  const followers = document.createElement('p');
+  followers.textContent = `Followers: ${user.followers}`;
+  cardInfo.appendChild(followers);
+
+  const following = document.createElement('p');
+  following.textContent = `Following: ${user.following}`;
+  cardInfo.appendChild(following);
+
+  const bio = document.createElement('p');
+  bio.textContent = `Bio: ${user.bio}`;
+  cardInfo.appendChild(bio);
+
+  return card;
+}
+
+// console.log(githubCard());
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
